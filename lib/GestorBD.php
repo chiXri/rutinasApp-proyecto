4ee1654da8e0022df53ed1a6bb148713377f0bb4
DemeRequena <161158329+DemeRequena@users.noreply.github.com>
2024-03-27 12:51:18 +0100

    fichero gestor base de datos

--- lib/GestorBD.php
+++ lib/GestorBD.php
@@ -12,6 +12,44 @@
          }
          return $conexion;
     }
+
+    private static function preparar($conexion, $consulta, $parametros){
+        
+      $preparada = $conexion->prepare($consulta);
+      
+      if ($parametros){
+          $tipos = "";
+          foreach($parametros as $parametro){
+              $tipos .= is_integer($parametro) ? "i" : "s";
+          }
+          $preparada->bind_param($tipos, ...$parametros);
+
+      }
+      return $preparada;
+  }
+
+  public static function consultaLectura($consulta, ...$parametros){
+
+      $conexion = self::conectar();
+      $retorno = array();
+      $preparada = self::preparar($conexion, $consulta, $parametros);
+      $preparada->execute();
+      $resultado = $preparada->get_result();
+
+      while ($fila = $resultado->fetch_assoc()){
+          array_push($retorno, $fila);
+      }
+
+      return $retorno;
+  }
+
+  public static function consultaEscritura ($consulta, ...$parametros){
+      $conexion = self::conectar();
+
+      $preparada = self::preparar($conexion, $consulta, $parametros);
+      $preparada->execute();
+
+  }
  }
 
 
