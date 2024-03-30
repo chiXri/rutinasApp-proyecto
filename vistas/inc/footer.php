bb5f1094cc7356fb2be10d9242baf7e2ab8e50e2
jvivas95 <jefferson.vivas.95@gmail.com>
2024-03-30 02:39:53 +0100

    Modificacion de rutas y creacion de inicio tras el loggin

--- vistas/inc/footer.php
+++ vistas/inc/footer.php
@@ -1,6 +1,6 @@
   <!-- FOOTER -->
   <footer class="container">
-    <p>&copy; 2017–2024 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    <p>&copy; 2017–2024 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a>  &middot; <a href="/rutinasApp-proyecto/vistas/contact.php">Contacto</a></p>
   </footer>
 </main>
 <script src="./assets/js/bootstrap.bundle.min.js"></script>
--- vistas/inc/header.php
+++ vistas/inc/header.php
@@ -1,15 +1,16 @@
 <!-- CABECERA -->    
 <header data-bs-theme="dark">
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <div class="container-fluid">
       <img src="/rutinasApp-proyecto/assets/img/logo.png" style="width: 10%; padding: 5px;"/>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+      <!--
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/rutinasApp-proyecto/vistas/inicio.php">HOME</a>
+              <a class="nav-link active" aria-current="page" href="/rutinasApp-proyecto/index.php">HOME</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/rutinasApp-proyecto/vistas/contact.php">CONTACTO</a>
@@ -20,6 +21,9 @@
         </ul>
        
       </div>
+      -->
+      <a href="/rutinasApp-proyecto/vistas/login.php"><button>Iniciar Sesión</button></a>
     </div>
+
   </nav>
 </header>
